337b2385b049c9dd678390ce3b1e3911c885c36a
RyUmarof <ryumarof@gmail.com>
2024-11-15 18:33:13 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let orqa = prompt('bakcground rangini kiriting')
+let text = prompt('radius')
+
+
+
+let txt = document.querySelector('div')
+
+txt.style.background = orqa;
+txt.style.borderRadius = `${text}px`
